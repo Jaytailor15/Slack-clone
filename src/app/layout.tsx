6aa4a0aa9a0b4cda1587896375694ab47118e394
Jay Tailor <jay8460568088@gmail.com>
2024-08-27 23:58:22 +0530

    workspace layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import { ConvexClientProvider } from "@/components/convex-client-provider";
+import { Modals } from "@/components/modals";
+import { Toaster } from "@/components/ui/sonner";
 import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -21,6 +23,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ConvexClientProvider>
+        <Toaster/>
+        <Modals/>
         {children}
         </ConvexClientProvider>
         </body>
