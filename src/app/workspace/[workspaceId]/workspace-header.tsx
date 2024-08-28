58779e4eb21c5f5557173745c8539e6b58bd3fd3
Jay Tailor <jay8460568088@gmail.com>
2024-08-29 02:50:00 +0530

    added join code

--- src/app/workspace/[workspaceId]/workspace-header.tsx
+++ src/app/workspace/[workspaceId]/workspace-header.tsx
@@ -10,6 +10,7 @@ import {
 import { ChevronDown, ListFilter, SquarePen } from "lucide-react";
 import { useState } from "react";
 import { Doc } from "../../../../convex/_generated/dataModel";
+import { InviteModal } from "./invite-modal";
 import { PreferencesModal } from "./preference-modal";
 
 
@@ -20,8 +21,15 @@ interface WorkspaceHeaderProps {
 
 export const WorkspaceHeader = ({ workspace, isAdmin }: WorkspaceHeaderProps) => {
     const [preferencesOpen, setPreferencesOpen] = useState(false);
+    const [inviteOpen, setInviteOpen] = useState(false);
     return (
         <>
+        <InviteModal
+        open ={inviteOpen}
+        setOpen={setInviteOpen}
+        name={workspace.name}
+        joinCode={workspace.joinCode}
+         />
         <PreferencesModal open={preferencesOpen} setOpen={setPreferencesOpen} initialValue={workspace.name}/>
         <div className="flex items-center justify-between px-4 h-[49px] gap-0.5">
             <DropdownMenu>
@@ -52,7 +60,7 @@ export const WorkspaceHeader = ({ workspace, isAdmin }: WorkspaceHeaderProps) =>
                             <DropdownMenuSeparator />
                             <DropdownMenuItem
                                 className="cursor-pointer py-2"
-                                onClick={() =>{}}
+                                onClick={() =>setInviteOpen(true)}
                             >
                                 Invite people to {workspace.name}
                             </DropdownMenuItem>
